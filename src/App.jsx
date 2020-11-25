963e5da89ce40a8f8096ad3316d5a8fc913d406c
sajad shafie <sajadice2@gmail.com>
2020-11-25 23:35:09 +0330

    frist commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+	return (
+		<div>
+			<header>
+				<a href="./">Header Shooping Cart</a>
+			</header>
+			<main>
+				<div>Products</div>
+			</main>
+			<footer>
+				<div>Footer is Here</div>
+			</footer>
+		</div>
+	);
+};
+export default App;
